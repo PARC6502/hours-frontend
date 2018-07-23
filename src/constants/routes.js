2f22eb0144aa68545eeab12b6f360d8d0b4e9362
parc6502 <ibrahim@serafy.co.uk>
2018-07-23 10:44:26 +0200

    added admin routes to routes.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -6,4 +6,8 @@ export const ORGANISATION_PAGE = '/organisation/:organisationId';
 export const SIGN_IN = '/sign-in';
 export const SIGN_UP = '/sign-up';
 export const MANAGE_ACCOUNT = '/manage-account';
-export const PROFILE = '/profile'
+export const PROFILE = '/profile';
+
+/* ADMIN routes */
+export const ADMIN_PAGE = '/admin';
+export const ADMIN_ADD_ORGANISATION = '/admin/ADD_ORGANISATION';
