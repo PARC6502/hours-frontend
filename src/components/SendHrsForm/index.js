f390536fc950be15f5ff0b6af5c91cb05998d21a
parc6502 <ibrahim@serafy.co.uk>
2018-07-27 16:57:48 +0200

    Drop spend functionality in send section

--- src/components/SendHrsForm/index.js
+++ src/components/SendHrsForm/index.js
@@ -1,43 +1,9 @@
-import React, { Component, Fragment } from 'react';
-import { Form } from 'semantic-ui-react';
+import React from 'react';
+
 
 import SendHrsToUser from './SendHrsToUser';
 
-class SendHrsForm extends Component {
-  state = {
-    sendingType: '',
-  };
-
-  onSendingTypeSelect = (evt, {value}) => {
-    const sendingType = value;
-    this.setState({ sendingType });
-  };
-
-  render() {
-    return (
-      <Fragment>
-        <Form>
-          <Form.Group inline>
-            <Form.Radio 
-              name="sendingType"
-              label='Spend on service or product' 
-              value='service' 
-              checked={this.state.sendingType === 'service'} 
-              onChange={this.onSendingTypeSelect} />
-            <Form.Radio
-              name="sendingType" 
-              label='Send to other user' 
-              value='user' 
-              checked={this.state.sendingType === 'user'} 
-              onChange={this.onSendingTypeSelect} />
-          </Form.Group>
-        </Form>
-        {this.state.sendingType === 'user'  
-        ? <SendHrsToUser />
-        : null}
-      </Fragment>
-    );
-  }
-}
+const SendHrsForm = (props) => 
+      <SendHrsToUser />
 
 export default SendHrsForm;
