038ec1b89374b25a547cb4a1212626ced9baa29d
parc6502 <ibrahim@serafy.co.uk>
2018-07-31 19:44:32 +0200

    Added photos and info to infomodal

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -1,22 +1,63 @@
-import React, { Component } from 'react'
-import { Grid, Button, Image, Modal } from 'semantic-ui-react'
+import React, { Component, Fragment } from 'react'
+import { Grid, Button, Image, Modal, Header } from 'semantic-ui-react'
 
-import nan from '../nan.jpg';
-import daniel from '../daniel.jpg';
+import aboutUs from '../images/jumpyHours.png';
+import howItWorks from '../images/helping.jpg';
+import logHours from '../images/logHours.png';
+import spendHours from '../images/shopSupport.png';
+import exchangeHours from '../images/greatTime.png';
+import keepTrack from '../images/ToolExchange.png';
+import supporters from '../images/supporters.png';
+
 import { FirebaseAuthUserContext } from './Session/FirebaseAuthUserProvider';
 
 const infoPanes = [
     {
-        text: 'Hours gives you wings',
-        image: nan,
+        header: `About Us`,
+        text: `Hours is a new time based cryptocurrency platform that gives a tangible value to voluntary sector and community activity, so that patrons, businesses and communities can support the hard-working people in nonprofits and in the local area to champion their work. Hours also unlocks innovative mutual aid and trust pathways between, otherwise unconnected, organisations and activities.`,
+        image: aboutUs,
     },
     {
-        text: 'Hours kills lizard people',
-        image: daniel,
+        header: `How it works`,
+        text: <Fragment>
+                <Header content='Work hours' size='small' />
+                Volunteer hours of your time at a non-profit or within the local community
+                <Header content='Log Hours' size='small' />
+                Record the hours you have worked and what you have done
+                <Header content='Spend Hours' size='small' />
+                Redeem your hours within the network
+            </Fragment>,
+        image: howItWorks,
     },
     {
-        text: 'All hail hours, the one true god',
-        image: nan,
+        header: `Log`,
+        text: `You can log the hours you have worked by clicking on the log hours button and picking the organisation from the drop down menu. A moderator from your organisation will need to approve your hours for you to receive them.`,
+        image: logHours,
+    },
+    {
+        header: `Spend`,
+        text: `Once you receive hours you will be able to spend these with our growing network of supporters who accept them. From cinema tickets to travel subsidiaries. This is a community currency, provided and spend in the community. Become a vendor or patron to help grow the network.`,
+        image: spendHours,
+    },
+    {
+        header: `Exchange`,
+        text: `You can also exchange hours by sending them directly to an individual as a tip, a token of gratitude or as a payment for some help they have provided. These personal transactions generate a digital receipt that keeps track of actions and exchanges. `,
+        image: exchangeHours,
+    },
+    {
+        header: `Keep track of things`,
+        text: `Everytime you borrow something, for example a hammer from the toolbox. Record the exchange with a token amount of hours, so users can track down borrowed items easily.`,
+        image: keepTrack,
+    },
+    {
+        header: `Our supporters and partners`,
+        text: `We would like to thank our partners in helping us develop the project.
+
+        Easa Croatia.
+        Rijeka 2020.
+        Party for the people.
+        `,
+        image: supporters,
     },
 ]
 
@@ -50,12 +91,17 @@ class InfoGallery extends Component {
         const {activeIndex} = this.state;
         return (
             <Grid>
+                <Grid.Row centered><Header content={panes[activeIndex].header} /></Grid.Row>
                 <Grid.Row>
                     <Grid.Column width={2} verticalAlign='middle'>
                         <Button basic icon='chevron left' onClick={this.onLeftClick} />
                     </Grid.Column>
-                    <Grid.Column width={6}><p>{panes[activeIndex].text}</p></Grid.Column>
-                    <Grid.Column width={6}><Image src={panes[activeIndex].image} /></Grid.Column>
+                    <Grid.Column width={6} verticalAlign='middle'>
+                        <p>{panes[activeIndex].text}</p>
+                    </Grid.Column>
+                    <Grid.Column width={6} verticalAlign='middle'>
+                        <Image src={panes[activeIndex].image} />
+                    </Grid.Column>
                     <Grid.Column width={2} verticalAlign='middle'>
                         <Button basic icon='chevron right' onClick={this.onRightClick} />
                     </Grid.Column>
@@ -114,8 +160,10 @@ class InfoModal extends Component {
         onClose={this.handleClose}
         size='small'
         closeIcon
+        id='infoModal'
+        
       >
-        <Modal.Content>
+        <Modal.Content style={{ backgroundColor:'#F5F5DC' }}>
             <InfoGallery panes={infoPanes} />  
         </Modal.Content>
       </Modal>
