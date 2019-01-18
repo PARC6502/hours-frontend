b510d686ea37a171255ae61ba2793363bbcffb20
parc6502 <ibrahim@serafy.co.uk>
2019-01-18 13:15:09 +0000

    Clean up edit image on EditProfile page

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -10,7 +10,10 @@ class EditProfile extends React.Component {
         bio: this.props.user.bio,
         image: null,
         imageUrl: this.props.user.image,
-        loading: false,
+        uploadingImage: false,
+        imageError: false,
+        imageSuccess: false,
+        bioLoading: false,
         bioSuccess: false,
         bioError: false
     }
@@ -20,7 +23,7 @@ class EditProfile extends React.Component {
     };
 
     saveBio = () => {
-        this.setState({ loading: true });
+        this.setState({ bioLoading: true });
         db.editUserBio(this.props.user.id, this.state.bio)
         .then(result => {
             console.log(result);
@@ -39,34 +42,55 @@ class EditProfile extends React.Component {
     onImageChange = (evt) => {
         const image = evt.target.files[0];
         const imageUrl = window.URL.createObjectURL(image);
-        this.setState({ image, imageUrl });
+        this.setState({ 
+            image, 
+            imageUrl,
+            imageError: false,
+            imageSuccess: false,
+        });
     }
 
+    // Removing progress bar for now as most images should be too small
     uploadImage = () => {
-        storage.uploadUserImage(this.props.user.id, this.state.image)
-        .then(snapshot => snapshot.ref.getDownloadURL())
-        .then(downloadUrl => db.editUserImage(this.props.user.id, downloadUrl))
-        .then(() => console.log('successfully added image'))
+        this.setState({ uploadingImage: true });
+        let uploadTask = storage.uploadUserImage(this.props.user.id, this.state.image);
+
+        let next = snapshot => {
+            let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log(progress);
+        }
+        let error = () => {
+            this.setState({ uploadingImage: false, imageError: true })
+        }
+        let complete = () => {
+            this.setState({ uploadingImage: false, imageSuccess: true })
+        }
+        
+        uploadTask.on('state_changed', next, error, complete);
     }
 
     render() {
         const bioChanged = (this.props.user.bio !== this.state.bio);
 
-        const imageChanged = (this.props.user.image !== this.state.image);
+        const imageChanged = (this.props.user.image !== this.state.imageUrl) && !this.state.imageSuccess;
 
         // TODO separate TextArea to avoid unecessarily rerendering whole page, method of measuring performance?
         return (
             <Fragment>
-                <Form>
-                    {this.state.imageUrl ? <Image src={this.state.imageUrl} /> : null}
+                <Form success={this.state.imageSuccess} error={this.state.imageError}>
+                    {this.state.imageUrl ? <Image size='small' centered src={this.state.imageUrl} /> : null}
                     <Form.Input
                         accept="image/*" 
                         type='file' label='Edit Profile Image' 
                         onChange={this.onImageChange} />
+                    <Message success content="Profile image successfully updated!" />
+                    <Message error content="There was a problem uploading your image, sorry about that!" />
+                   
                     <Form.Button
                         onClick={this.uploadImage}
                         color='green' 
                         fluid
+                        loading={this.state.uploadingImage}
                         disabled={!imageChanged} >Save</Form.Button>
                 </Form>
                 <Form success={this.state.bioSuccess} error={this.state.bioError}>
@@ -81,6 +105,7 @@ class EditProfile extends React.Component {
                         onClick={this.saveBio}
                         color='green' 
                         fluid
+                        loading={this.state.bioLoading}
                         disabled={!bioChanged} >Save</Form.Button>
                 </Form>
             </Fragment>
