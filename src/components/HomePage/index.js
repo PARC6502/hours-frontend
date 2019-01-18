2a9c22f5b7a897134ef87a3d41eaf98aa8281856
parc6502 <ibrahim@serafy.co.uk>
2019-01-18 14:25:03 +0000

    added dev warning message

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
-import { Segment } from 'semantic-ui-react';
+import { Segment, Message } from 'semantic-ui-react';
 
 import EventFeed from './EventFeed';
 
+const DevWarningMessage = () => 
+	<Message warning>This is a development build for testing, hours logged here are going into a different database.</Message>
+
 const HomePage = () =>
 	<Fragment>
+		{process.env.REACT_APP_DEVELOP === 'false' ? null : <DevWarningMessage />}
 		<Segment>Welcome to the hours project dude</Segment>
 		<EventFeed />
 	</Fragment>
