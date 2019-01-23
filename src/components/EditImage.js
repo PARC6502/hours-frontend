2c1122aedad8de92703ade585e4e832b111fa5c2
parc6502 <ibrahim@serafy.co.uk>
2019-01-23 18:17:22 +0000

    Separated EditImage into its own component

---
+++ src/components/EditImage.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Form, Image, Message } from 'semantic-ui-react';
+
+import { storage } from '../firebase';
+
+class EditImage extends React.Component {
+    state = {
+        image: null,
+        imageUrl: this.props.imageUrl,
+        loading: false,
+        success: false,
+        error: false,
+    }
+
+    onChange = (evt) => {
+        const image = evt.target.files[0];
+        const imageUrl = window.URL.createObjectURL(image);
+        this.setState({ 
+            image, 
+            imageUrl,
+            error: false,
+            success: false,
+        });
+    }
+
+    // Removing progress bar for now as most images should be too small
+    uploadImage = () => {
+        this.setState({ loading: true });
+        storage.uploadUserImage(this.props.userId, this.state.image)
+        .then(() => this.setState({ loading: false, success: true }))
+        .catch(() => this.setState({ loading: false, error: true }))
+        // let uploadTask = storage.uploadUserImage(this.props.userId, this.state.image);
+        // let next = snapshot => {
+        //     let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        //     console.log(progress);
+        // }
+        // let error = () => {
+        //     this.setState({ loading: false, error: true })
+        // }
+        // let complete = () => {
+        //     this.setState({ loading: false, success: true })
+        // }
+        // uploadTask.on('state_changed', next, error, complete);
+    }
+    
+    render() {
+        const imageChanged = (this.props.imageUrl !== this.state.imageUrl) && !this.state.success;
+
+        return (
+            <Form success={this.state.success} error={this.state.error}>
+                {this.state.imageUrl ? <Image size='small' centered src={this.state.imageUrl} /> : null}
+                <Form.Input
+                    accept="image/*" 
+                    type='file' label='Edit Profile Image' 
+                    onChange={this.onChange} />
+                <Message success content="Profile image successfully updated!" />
+                <Message error content="There was a problem uploading your image, sorry about that!" />
+                
+                <Form.Button
+                    onClick={this.uploadImage}
+                    color='green' 
+                    fluid
+                    loading={this.state.loading}
+                    disabled={!imageChanged} >Save</Form.Button>
+            </Form>
+        )
+    }
+}
+
+export default EditImage
--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,104 +1,15 @@
 import React, { Fragment } from 'react';
-import { Form, Image, Message } from 'semantic-ui-react';
-
-import { storage } from '../firebase';
 import { FirebaseAuthUserContext } from './Session/FirebaseAuthUserProvider';
 import withAuthorization from './Session/withAuthorization';
 import EditBio from './EditBio';
+import EditImage from './EditImage';
 
-class EditProfile extends React.Component {
-    state = {
-        bio: this.props.user.bio,
-        image: null,
-        imageUrl: this.props.user.image,
-        uploadingImage: false,
-        imageError: false,
-        imageSuccess: false,
-        bioLoading: false,
-        bioSuccess: false,
-        bioError: false
-    }
 
-    // onBioChange = (evt, { value }) => {
-    //     this.setState({ bio: value });
-    // };
-
-    // saveBio = () => {
-    //     this.setState({ bioLoading: true });
-    //     db.editUserBio(this.props.user.id, this.state.bio)
-    //     .then(result => {
-    //         console.log(result);
-    //         this.setState({ bioSuccess: true });
-    //     })
-    //     .catch(error => {
-    //         console.log(error);
-    //         this.setState({ bioError: true });
-    //     })
-    //     .then(() => { 
-    //         this.setState({ loading: false })
-    //         console.log(this.props.user.bio)
-    //     })
-    // }
-
-    onImageChange = (evt) => {
-        const image = evt.target.files[0];
-        const imageUrl = window.URL.createObjectURL(image);
-        this.setState({ 
-            image, 
-            imageUrl,
-            imageError: false,
-            imageSuccess: false,
-        });
-    }
-
-    // Removing progress bar for now as most images should be too small
-    uploadImage = () => {
-        this.setState({ uploadingImage: true });
-        let uploadTask = storage.uploadUserImage(this.props.user.id, this.state.image);
-
-        let next = snapshot => {
-            let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log(progress);
-        }
-        let error = () => {
-            this.setState({ uploadingImage: false, imageError: true })
-        }
-        let complete = () => {
-            this.setState({ uploadingImage: false, imageSuccess: true })
-        }
-        
-        uploadTask.on('state_changed', next, error, complete);
-    }
-
-    render() {
-        // const bioChanged = (this.props.user.bio !== this.state.bio);
-
-        const imageChanged = (this.props.user.image !== this.state.imageUrl) && !this.state.imageSuccess;
-
-        // TODO separate TextArea to avoid unecessarily rerendering whole page, method of measuring performance?
-        return (
-            <Fragment>
-                <Form success={this.state.imageSuccess} error={this.state.imageError}>
-                    {this.state.imageUrl ? <Image size='small' centered src={this.state.imageUrl} /> : null}
-                    <Form.Input
-                        accept="image/*" 
-                        type='file' label='Edit Profile Image' 
-                        onChange={this.onImageChange} />
-                    <Message success content="Profile image successfully updated!" />
-                    <Message error content="There was a problem uploading your image, sorry about that!" />
-                   
-                    <Form.Button
-                        onClick={this.uploadImage}
-                        color='green' 
-                        fluid
-                        loading={this.state.uploadingImage}
-                        disabled={!imageChanged} >Save</Form.Button>
-                </Form>
-                <EditBio bio={this.props.user.bio} userId={this.props.user.id} />
-            </Fragment>
-        );
-    }
-}
+const EditProfile = (props) => 
+    <Fragment>
+        <EditImage imageUrl={props.user.image}  userId={props.user.id} />
+        <EditBio bio={props.user.bio} userId={props.user.id} />
+    </Fragment>
 
 const EditProfilePage = () => 
 	<FirebaseAuthUserContext.Consumer>
