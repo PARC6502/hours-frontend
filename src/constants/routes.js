ae47283701919403e26a5df44b8926ceb7180931
ldpohl1 <62161074+ldpohl1@users.noreply.github.com>
2020-04-25 19:48:13 +0100

    Solvi changes to constants
    Solvi changes to constants

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,5 @@
 export const HOME_PAGE = '/';
+export const ABOUT_PAGE = "/about";
 export const ADD_HRS = '/add-hrs';
 export const SEND_HRS = '/send-hrs';
 export const USER_PAGE = '/user/:userId';
