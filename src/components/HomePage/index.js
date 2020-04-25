d3aa3081652aa501c5c3c6df6db99b3723e4cfa8
ldpohl1 <62161074+ldpohl1@users.noreply.github.com>
2020-04-25 09:42:39 +0100

    Update index.js
    Added Solvi's text

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -5,7 +5,17 @@ import EventFeed from "./EventFeed";
 
 const HomePage = () => (
   <Fragment>
-    <Segment>Welcome to the mutual impact feed</Segment>
+    <Segment>
+      <h3>Welcome to the mutual impact feed</h3>
+      <p>
+        Please note: this feed is in beta and is only populated with data that has been
+        collected and uploaded.
+      </p>
+      <p>
+        An absence of data for a given area should not be interpreted to mean
+        that work hasn't happened, just that it hasn't been recorded yet! :)
+      </p>
+    </Segment>
     <EventFeed />
   </Fragment>
 );
