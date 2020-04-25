68bc1ed8b36c623aa90be0aa7da60b0b5d614788
Solvi Goard <solvi.goard@gmail.com>
2020-04-25 17:02:47 +0100

    Fix: add about to nav

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -7,45 +7,46 @@ import AccountDropdown from './AccountDropdown';
 import { FirebaseAuthUserContext } from '../Session/FirebaseAuthUserProvider';
 import * as routes from '../../constants/routes';
 
-// const Navigation = () => 
+// const Navigation = () =>
 //     <FirebaseAuthUserContext.Consumer>
 //         {user => user.isUserSignedIn && !user.pendingUser ? <NavigationAuth user={user} /> : <NavigationNonAuth />}
 //     </FirebaseAuthUserContext.Consumer>
 
 // const NavigationAuth = (props) => {
 //     const isAdmin = props.user.role === 'ADMIN';
-    
+
 //     return (
 //         <Menu stackable>
 //         <Menu.Item exact as={NavLink} to={routes.HOME_PAGE}>Homepage</Menu.Item>
 //         <Menu.Item as={NavLink} to={routes.ADD_HRS}>Log Hrs</Menu.Item>
-//         <Menu.Item as={NavLink} to={routes.SEND_HRS}>Send Hrs</Menu.Item>  
-//         <Menu.Item as={NavLink} to={routes.PROFILE}>Your Profile</Menu.Item>  
+//         <Menu.Item as={NavLink} to={routes.SEND_HRS}>Send Hrs</Menu.Item>
+//         <Menu.Item as={NavLink} to={routes.PROFILE}>Your Profile</Menu.Item>
 //         {isAdmin ? <Menu.Item as={NavLink} to={routes.ADMIN_PAGE}>Admin Page</Menu.Item> : null}
 //         <Menu.Menu position='right'>
 //             <SearchMembers className="right aligned item" />
 //             <AccountDropdown user={props.user} />
 //             {/* <Menu.Item><Button onClick={auth.doSignOut}>Log out</Button></Menu.Item> */}
-//         </Menu.Menu>  
+//         </Menu.Menu>
 //     </Menu>
 //     )
 // }
-    
 
-// const NavigationNonAuth = () => 
+
+// const NavigationNonAuth = () =>
 //     <Menu>
 //         <Menu.Item exact as={NavLink} to={routes.HOME_PAGE}>Homepage</Menu.Item>
-//         <Menu.Item as={NavLink} to={routes.SIGN_IN}>Sign in</Menu.Item> 
+//         <Menu.Item as={NavLink} to={routes.SIGN_IN}>Sign in</Menu.Item>
 //         <Menu.Item position='right' fitted>
 //             <Button as={NavLink} to={routes.SIGN_UP} fluid primary>Sign up</Button>
-//         </Menu.Item> 
+//         </Menu.Item>
 //     </Menu>
 
 
-const NonAuthMenuItems = () => 
+const NonAuthMenuItems = () =>
     <Fragment>
         <Menu.Item exact as={NavLink} to={routes.HOME_PAGE}>Homepage</Menu.Item>
         <Menu.Item as={NavLink} to={routes.SIGN_IN}>Sign in</Menu.Item> 
+        <Menu.Item as={NavLink} to={routes.ABOUT_PAGE}>About</Menu.Item>
         <Menu.Item position='right' fitted>
             <Button as={NavLink} to={routes.SIGN_UP} fluid primary>Sign up</Button>
         </Menu.Item>
@@ -53,27 +54,27 @@ const NonAuthMenuItems = () =>
 
 const AuthMenuItems = (props) => {
     const isAdmin = props.user.role === 'ADMIN';
-    
+
     return (
         <Fragment>
             <Menu.Item exact as={NavLink} to={routes.HOME_PAGE}>Homepage</Menu.Item>
             <Menu.Item as={NavLink} to={routes.ADD_HRS}>Log Hrs</Menu.Item>
-            <Menu.Item as={NavLink} to={routes.SEND_HRS}>Send Hrs</Menu.Item>  
-            <Menu.Item as={NavLink} to={routes.PROFILE}>Your Profile</Menu.Item>  
+            <Menu.Item as={NavLink} to={routes.SEND_HRS}>Send Hrs</Menu.Item>
+            <Menu.Item as={NavLink} to={routes.PROFILE}>Your Profile</Menu.Item>
             {isAdmin ? <Menu.Item as={NavLink} to={routes.ADMIN_PAGE}>Admin Page</Menu.Item> : null}
             <Menu.Menu position='right'>
                 <SearchMembers className="right aligned item" />
                 <AccountDropdown user={props.user} />
                 {/* <Menu.Item><Button onClick={auth.doSignOut}>Log out</Button></Menu.Item> */}
-            </Menu.Menu>  
+            </Menu.Menu>
         </Fragment>
     )
 }
 
 const MenuItems = () =>
     <FirebaseAuthUserContext.Consumer>
-        {user => user.isUserSignedIn && !user.pendingUser 
-        ? <AuthMenuItems user={user} /> : 
+        {user => user.isUserSignedIn && !user.pendingUser
+        ? <AuthMenuItems user={user} /> :
         <NonAuthMenuItems />}
     </FirebaseAuthUserContext.Consumer>
 
@@ -90,7 +91,7 @@ const SidebarMenu = (props) => (
     </Sidebar>
 )
 
-const BarMenu = () => 
+const BarMenu = () =>
     <Menu fixed='top'>
         <MenuItems />
     </Menu>
@@ -106,7 +107,7 @@ class ResponsiveNavigation extends React.Component {
         if (locationChanged) this.hideSidebar();
     }
 
-    handleButtonClick = () => 
+    handleButtonClick = () =>
         this.setState(prevState => ({ sidebarActive: !prevState.sidebarActive }));
 
     hideSidebar = () => this.setState({ sidebarActive: false });
@@ -120,12 +121,12 @@ class ResponsiveNavigation extends React.Component {
                             <Icon name='bars' />
                         </Menu.Item>
                     </Menu>
-                    <SidebarMenu visible={this.state.sidebarActive} onHide={this.hideSidebar} /> 
+                    <SidebarMenu visible={this.state.sidebarActive} onHide={this.hideSidebar} />
                 </Responsive>
                 <Responsive minWidth={Responsive.onlyTablet.minWidth}>
                     <BarMenu />
-                </Responsive>                 
-                
+                </Responsive>
+
             </Fragment>
         )
     }
